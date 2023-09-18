db0f5d56fe26b3dc908908148fb26f37d3d577ef
David L.E <59020919+neldivad@users.noreply.github.com>
2023-09-19 01:00:40 +0800

    Update charting.py

--- charting.py
+++ charting.py
@@ -10,11 +10,26 @@ from typing import Union
 from typing import Optional
 from PIL import Image
 
-from utils.globals import ColorDiscrete
-
 #---
 # chart config
 #---
+class ColorDiscrete:
+  tableau = ['#4E79A7', '#F28E2B', '#E15759', '#76B7B2', '#59A14F', '#EDC948', '#B07AA1', '#FF9DA7', '#9C755F', '#BAB0AC']
+  colorbrewer = ['#E41A1C', '#377EB8', '#4DAF4A', '#984EA3', '#FF7F00', '#FFFF33', '#A65628', '#F781BF', '#999999']
+  google = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722'] 
+  d3 = ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#E377C2', '#7F7F7F', '#BCBD22', '#17BECF']
+  ilo = ['#0094D2', '#E6007E', '#F47D30', '#212121', '#F0F0F0'] 
+  okabe_ito = ['#E69F00', '#56B4E9', '#009E73', '#F0E442', '#0072B2', '#D55E00', '#CC79A7', '#000000', '#F0E442', '#D55E00']
+  economist = ['#E3120B', '#4C4C4C', '#6D6E71', '#8E8D8F', '#AAAAB2', '#B6B4B5', '#C1BDBA', '#C9C5B6', '#D2CDC2', '#D9D5CD']
+  mckinsey = ['#4A90E2', '#F5A623', '#7ED321', '#D0021B', '#417505', '#BD10E0', '#50E3C2', '#F8E71C', '#F78B00', '#000000']
+  deloitte = ['#00A5DC', '#49276D', '#8A0000', '#007681', '#9A7227', '#84BE41', '#7C7474', '#D12F2F', '#F8931D', '#000000']
+  nhk_jp = ['#E60012', '#579D1C', '#4B1E66', '#1B255E', '#1E4896', '#00A0E9', '#009944', '#6CBB5A', '#D1C000', '#E39800']
+  abc_aus = ['#E14100', '#008C45', '#00AEEF', '#7600A1', '#80C342', '#005BB5', '#93C6E0', '#F15A29', '#DF7C00', '#00B5E2']
+
+  gecko7 = ['#004457', '#567583', '#9baab1', '#e2e2e2', '#b8e8e3', '#83ede3', '#05f1e3']
+  gecko5 = ['#004457', '#798f9a', '#e2e2e2', '#a0ebe3', '#05f1e3']
+  gecko3 = ['#004457', '#798f9a', '#05f1e3']
+    
 GECKO_LOGO = Image.open("./resources/BlackShortText_Logo_Horizontal-long.png")
 
 def watermark_settings():
